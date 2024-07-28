7a49ac7986351d7c247cd860fba085ab2f38b727
JuanPabaz <jpgthf@gmail.com>
2024-07-28 17:50:24 -0500

    feat: class 19 filter function

--- clase17.py
+++ clase17.py
@@ -17,13 +17,20 @@ items = [
     }
 ]
 
-prices = list(map(lambda product: product['price'],items))
+items2 = [
+    {
+        'product':'Camisa',
+        'price':100
+    }
+]
+
+prices = list(map(lambda product: product['price'],items2))
 print(prices)
 def add_taxes(item):
     item['taxes'] = item['price'] * .19
     return item
 
-items_with_taxes_list = list(map(add_taxes,items))
+items_with_taxes_list = list(map(add_taxes,items2))
 print(items_with_taxes_list)
-items_with_taxes_dict = {item['product']: item['price'] + item['taxes'] for item in items_with_taxes_list}
+items_with_taxes_dict = dict(items_with_taxes_list)
 print(items_with_taxes_dict)
