356e5597d43cecd9a7ca145cba152f6bd62912d2
JuanPabaz <jpgthf@gmail.com>
2024-07-29 12:02:25 -0500

    feat: country population

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
 import utils
+import read_csv
+import charts
 
-data = [
-    {
-        'Country':'Colombia',
-        'Population':50
-    },
-    {
-        'Country':'Bolivia',
-        'Population':30
-    }
-]
 
 def run():
-    country, population = utils.get_population()
-    print(country,population)
+    data = read_csv.read_csv("./app/world_population.csv")
 
-    country = input('Escribe un pais: ')
-    result = utils.population_by_country(data,country)
-    print(result)
+    country = input("Escribe un pais: ")
+    result = utils.population_by_country(data, country)
+    if len(result) > 0:
+        country = result[0]
+        keys, values = utils.get_population(country)
+        charts.generate_bar_chart(keys, values)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
