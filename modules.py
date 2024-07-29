969916a54cfeb6e2d19afcc0eab5de69af2b3249
JuanPabaz <jpgthf@gmail.com>
2024-07-28 19:03:39 -0500

    feat: collections

--- modules.py
+++ modules.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import time
+import collections
 
 print(sys.path)
 text = 'Mi numero de telefono es 300 8298941, el codigo del pais es +57, mi numero de la suerte es el 3.'
@@ -9,3 +10,8 @@ time_now = time.time()
 local_date = time.localtime()
 result = time.asctime()
 print(result)
+
+numbers = [1,2,3,4,21,2,21,2,1,3,4]
+counter = collections.Counter(numbers)
+print(counter)
+
