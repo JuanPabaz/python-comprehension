862b15880706a602e5636943217104849c804999
JuanPabaz <jpgthf@gmail.com>
2024-07-28 21:28:56 -0500

    feat: read csv

--- clase17.py
+++ clase17.py
@@ -24,13 +24,17 @@ items2 = [
     }
 ]
 
-prices = list(map(lambda product: product['price'],items2))
-print(prices)
+prices = list(map(lambda product: product,items2))
 def add_taxes(item):
     item['taxes'] = item['price'] * .19
     return item
 
 items_with_taxes_list = list(map(add_taxes,items2))
+keys = [key for item in items_with_taxes_list for key in item.keys()]
+values = [key for item in items_with_taxes_list for key in item.values()]
+print(values)
+print(keys)
+iterable = zip(keys,values)
+product_dict = {key:value for key, value in iterable}
+print(product_dict)
 print(items_with_taxes_list)
-items_with_taxes_dict = dict(items_with_taxes_list)
-print(items_with_taxes_dict)
